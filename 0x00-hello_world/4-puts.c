106b198a1c3c68677d3184157f3ce04742653f2e
kedist <kidytt@yahoo.com>
2023-07-13 09:41:13 -0700

    yap

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/**
- *main - entry point 
- *return: always 0 (success)
- * */
+/* main - entry point Return: Always 0 (Success) */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
