f7948673decbf73c1bf6cd5e8f4f6f66701e96cf
kedist <kidytt@yahoo.com>
2023-07-13 13:43:53 -0700

    sup

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main  - does my stuff
+* Description:does my stuff explained
+* Return:Always(Success);
+*/
+
+int main(void)
+{
+write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
