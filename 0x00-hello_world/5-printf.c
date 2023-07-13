e85e893dd6348e587cf8b8c0bafb0591a08a8b21
kedist <kidytt@yahoo.com>
2023-07-13 12:23:05 -0700

    g

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main  - does my stuff
+* Description:does my stuff explained
+* Return:Always(Success);
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
