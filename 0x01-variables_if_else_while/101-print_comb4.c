56c7ef44bf88431199fb300acf5dc8aad8a8c4fc
kedist <kidytt@yahoo.com>
2023-07-14 12:31:25 -0700

    ll

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,26 +10,23 @@ int main(void)
 
 	int c, i, j;
 
-	for (c = '0'; c <= '9' ; c++)
+	for (c = '0'; c <= 7 ; c++)
 	{
-		for (i = '0'; i <= '9' ; i++)
+		for (i = '0'; i <= 8 ; i++)
 		{
-			for (j = '0'; j <= '9' ; j++)
+			for (j = 0; j <= 9 ; j++)
 			{
 				if (c < i && i < j)
 			{
 				putchar(c);
 				putchar(i);
 				putchar(j);
-			}
-				if (c != '7')
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
+		}	
 	}
 	putchar('\n');
 	return (0);
-}
+}	
