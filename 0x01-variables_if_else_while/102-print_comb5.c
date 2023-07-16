8b78bf562dc9df17ddb4080c0ab7e7a1e819ba70
kedist <kidytt@yahoo.com>
2023-07-16 15:27:56 -0700

    new line

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,30 +8,23 @@ int main(void)
 {
 	int c, i;
 
-	for (c = 0; c < 98; c++)
+	for (c = 0; c <= 98; c++)
 	{
-		for (i = c + 1; i < 99; i++)
+		for (i = c + 1; i <= 99; i++)
 		{
 		putchar((c / 10) + '0');
 		putchar((c % 10) + '0');
 		putchar(' ');
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
-	
 		if (c == 98 && i == 99)
-		putchar((c / 10) + '0');
-                putchar((c % 10) + '0');
-                putchar(' ');
-                putchar((i / 10) + '0');
-                putchar((i % 10) + '0');
-
 			break;
+
 		putchar(',');
-                putchar(' ');
+		putchar(' ');
 		}
-	
-                
 	}
 	putchar('\n');
+
 	return (0);
 }
