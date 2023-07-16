7e7e495fc33a27adcf33113680c78466edef0529
kedist <kidytt@yahoo.com>
2023-07-16 01:28:23 -0700

    linter

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main  - does my stuff
+* Description:does my stuff explained
+* Return:Always(Success);
+*/
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l--)
+		putchar(l);
+
+	putchar('\n');
+
+	return (0);
+}
