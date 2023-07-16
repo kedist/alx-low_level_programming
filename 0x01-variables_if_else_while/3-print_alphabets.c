0e1adbce758b3a00ff78580a40553355a40b877a
kedist <kidytt@yahoo.com>
2023-07-16 01:06:04 -0700

    nt coooollo linter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main  - does my stuff
+* Description:does my stuff explained
+* Return:Always(Success);
+*/
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	putchar(l);
+
+	for (l = 'A'; l <= 'Z'; l++)
+	putchar(l);
+
+	putchar('\n');
+
+	return (0);
+}
