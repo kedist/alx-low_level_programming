1548a86bdc198b5bda8e1af71a02713168901502
kedist <kidytt@yahoo.com>
2023-07-18 13:54:03 -0700

    were

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main  - does my stuff
+* 
+* Description:does my stuff explained
+* Return:Always(Success);
+*/
+int main(void)
+{
+	int l;
+	
+	l = 0;
+	positive_or_negative(l);
+
+	return (0);
+}
+
+
+
+
