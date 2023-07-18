99f145379c335e7b769b39ef91ad68094fbfc783
kedist <kidytt@yahoo.com>
2023-07-18 11:00:16 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _islower  - does my stuff
+* Description:does my stuff explained
+* Return:Always(Success);
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
