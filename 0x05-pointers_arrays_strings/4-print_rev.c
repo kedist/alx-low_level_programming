c653e4a081aeab82b5fe4a8c567fb38ea0140c38
kedist <kidytt@yahoo.com>
2023-07-24 10:10:48 -0700

    lenegaba

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* print_rev - prints a string in reverse
+* @s: string to print
+*/
+
+void print_rev(char *s)
+{
+int len = 0, index = 0;
+len = _strlen(s);
+
+for (index = len - 1; index >= 0; index--)putchar(s[index]);
+
+putchar('\n');
+}
+
+int _strlen(char *s)
+{
+int count, i;
+i = 0;
+for (count = 0; s[count] != '\0'; count++)
+i++;
+
+return (i);
+}
+
