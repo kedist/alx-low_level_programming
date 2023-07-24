01c2e50d28cb39e12f184d56012c16cc116eca01
kedist <kidytt@yahoo.com>
2023-07-24 10:14:42 -0700

    drin

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,8 @@ void print_rev(char *s)
 int len = 0, index = 0;
 len = _strlen(s);
 
-for (index = len - 1; index >= 0; index--)putchar(s[index]);
+for (index = len - 1; index >= 0; index--)
+putchar(s[index]);
 
 putchar('\n');
 }
