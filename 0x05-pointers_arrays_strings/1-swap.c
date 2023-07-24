8721a43f517a30cb294b4660df6c7c452d23dae3
kedist <kidytt@yahoo.com>
2023-07-24 04:04:16 -0700

    who os

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*swap_int - swap two values 
+*@a: first value
+*@b: second value 
+*Return: nothing
+*/
+void swap_int(int *a, int *b)
+{
+int tmp = *a;
+*a = *b;
+*b = tmp;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_strlen - returns the lengt
+ * @s: str
+ *Return: returns l;
+ */
+int _strlen(char *s)
+{
+int  i = 0;
+
+for (i= 0; s[i] != '\0'; i++)
+i++;
+
+return (i);
+}
