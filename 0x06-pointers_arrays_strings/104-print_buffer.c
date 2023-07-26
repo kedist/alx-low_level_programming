5ecf614a77cfba0d55507e79e96a30840835d416
kedist <kidytt@yahoo.com>
2023-07-26 12:25:19 -0700

    cotedivoir

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_buffer - print a buffer
+ * @b: string 
+ * @size: buffer size
+ * Return: noting!
+ */
+void print_buffer(char *b, int size)
+{
+	int i, counter = 0, size2 = size;
+	char *ptr = b;
+
+	if (size == 0)
+		printf("\n");
+	while (size > 0)
+	{
+		printf("%08x:", counter);
+		i = 0;
+		while (size > 0 && i < 10)
+		{
+			if (!(i % 2))
+				printf(" ");
+			printf("%02x", *ptr++);
+			i++;
+			size--;
+			counter++;
+		}
+		while (counter % 10 != 0)
+		{
+			if (!(counter % 2))
+				printf(" ");
+			printf("  ");
+			counter++;
+		}
+		printf(" ");
+		i = 0;
+		while (size2 > 0 && i < 10)
+		{
+			if (*b > 31 && *b != 127)
+				printf("%c", *b);
+			else
+				printf(".");
+			size2--;
+			i++;
+			b++;
+		}
+		printf("\n");
+	}
+}
