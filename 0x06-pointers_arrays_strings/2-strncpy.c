0d471c30ad38436e282d0b53eb04c6ad18405fbd
kedist <kidytt@yahoo.com>
2023-07-26 11:06:29 -0700

    what

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,22 @@
-#include <unistd.h>
 #include "main.h"
 /**
-* main  - does my stuff
-* Description:does my stuff explained
-* Return:Always(Success);
-*/
+ * _strncpy - copying two strings
+ * @dest : pointer
+ * @src : source pointer
+ * @n : integer variable
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
 
+	for (; a < n && src[a] != '\0'; a++)
+		;
+	for (; b < a && src[b] != '\0'; b++)
+	{
+		dest[b] = src[b];
+	}
+	for (; b < n; b++)
+		dest[b] = '\0';
+	return (dest);
+}
