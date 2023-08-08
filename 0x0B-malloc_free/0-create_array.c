3486c61ebe4f12065f703b569bb5519bebf36741
kedist <kidytt@yahoo.com>
2023-08-08 11:30:54 -0700

    if u uns

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* main  - does my stuff
+* @size - takea chaars
+* @c - int takes
+* Description:does my stuff explained
+* Return:Always(Success);
+*/
+
+char *create_array(unsigned int size, char c)
+{
+
+char  *ac;
+unsigned int i;
+
+if (size == 0)
+return (NULL);
+
+ac =  malloc(sizeof(char) * size);
+if (ac == NULL)
+return (NULL);
+
+for(i = 0; i < size; i++)
+ac[i] = c;
+
+return (ac);
+}
