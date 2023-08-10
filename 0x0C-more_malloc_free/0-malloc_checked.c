f318c88b12299ad19c1ad87fa9698a31ba8271b6
kedist <kidytt@yahoo.com>
2023-08-10 13:31:35 -0700

    Sdfdsf

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* main  - does my stuff
+* @b -int passed allocated
+* Description:does my stuff explained
+* Return:Always(Success);
+*/
+
+void *malloc_checked(unsigned int b)
+{
+unsigned int *r;
+
+r = malloc(b);
+if (r == NULL)	
+exit (1);
+
+return (r);
+}
