734e3c585f32062e3b2bba47f4077cb2cf086705
kedist <kidytt@yahoo.com>
2023-08-14 08:12:46 -0700

    asdfasdf

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main  - does my stuff
+* Description:does my stuff explained
+* Return:Always(Success);
+*/
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
