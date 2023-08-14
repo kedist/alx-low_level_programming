75ba2d785ed15db0f1bac6ce4dcf78d4e71b13aa
kedist <kidytt@yahoo.com>
2023-08-14 09:31:31 -0700

    asfadsf

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* print_dog - does my stuff
+* @d: dog sruct
+* Description:does my stuff explained
+* Return:Nothing (Success);
+*/
+
+void print_dog(struct dog *d)
+{
+if (d)
+{
+	if (!(d->name))
+	printf("Name: (nil)\n");
+	else
+	printf("Name: %s\n", d->name);
+	if (!(d->age))
+	printf("Age: (nil)\n");
+	else
+	printf("Age: %f\n", d->age);
+	if (!(d->owner))
+	printf("Owner: (nil)\n");
+	else
+	printf("Owner: %s\n", d->owner);
+}
+}
